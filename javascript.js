89083cc5d052b0f416f1d6b758ca9ead15da3acf
kaue Rodrigues <kauerodrigues035@gmail.com>
2026-04-06 23:34:28 -0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,14 +1,24 @@
 document.getElementById('menu-butao').addEventListener('click', function () {
     const lateral = document.getElementById('lateral');
+    const overlay = document.getElementById('overlay'); 
 
     lateral.classList.toggle('active');
+    overlay.classList.toggle('active'); // Ativa/desativa o blur junto com a barra
 
-    if(lateral.classList.contains('active')) {
+   
+    if (lateral.classList.contains('active')) {
         this.textContent = '☰';
-    } else { 
+    } else {                          //ABRE E FECHA O MENU
         this.textContent = '☰';
     }
-    });
+});
 
-
+//  Fecha o menu se clicar no desfoque (fora da barra)
+document.getElementById('overlay').addEventListener('click', function () {
+    const lateral = document.getElementById('lateral');
+    const btn = document.getElementById('menu-butao');
     
+    lateral.classList.remove('active');
+    this.classList.remove('active');
+    btn.textContent = '☰';
+});
